e256f705e2848a7a8d6a48fcf89344b07528bd04
Im-JustAChillGuy <justforfunbruh1234@gmail.com>
2026-06-18 16:10:09 +0530

    Update ConfigScreen.java

--- src/client/java/com/example/client/ConfigScreen.java
+++ src/client/java/com/example/client/ConfigScreen.java
@@ -1,24 +1,19 @@
 package com.example.client;
-
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.CycleButton;
 import net.minecraft.network.chat.Component;
-
 public class ConfigScreen extends Screen {
     private final Screen parent;
     private final ModConfig config = ModConfig.get();
-
     public ConfigScreen(Screen parent) {
         super(Component.literal("Any Last Words - Config"));
         this.parent = parent;
     }
-
     @Override
     protected void init() {
         int centerX = this.width / 2;
         int startY = this.height / 4;
-
         // Enable/disable toggle
         addRenderableWidget(CycleButton.onOffBuilder(config.enabled)
             .create(centerX - 100, startY, 200, 20,
@@ -27,7 +22,6 @@ public class ConfigScreen extends Screen {
                     config.enabled = value;
                     ModConfig.save();
                 }));
-
         // Show dimension toggle
         addRenderableWidget(CycleButton.onOffBuilder(config.showDimension)
             .create(centerX - 100, startY + 30, 200, 20,
@@ -36,7 +30,6 @@ public class ConfigScreen extends Screen {
                     config.showDimension = value;
                     ModConfig.save();
                 }));
-
         // Precise coords toggle
         addRenderableWidget(CycleButton.onOffBuilder(config.preciseCoords)
             .create(centerX - 100, startY + 60, 200, 20,
@@ -45,17 +38,15 @@ public class ConfigScreen extends Screen {
                     config.preciseCoords = value;
                     ModConfig.save();
                 }));
-
         // Done button
         addRenderableWidget(Button.builder(
             Component.literal("Done"),
-            button -> this.minecraft.setScreen(parent))
+            button -> this.minecraft.gui.setScreen(parent))
             .bounds(centerX - 100, startY + 110, 200, 20)
             .build());
     }
-
     @Override
     public void onClose() {
-        this.minecraft.setScreen(parent);
+        this.minecraft.gui.setScreen(parent);
     }
 }
